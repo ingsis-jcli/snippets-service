9c726d76192a0f7ac8482a043165647ae004ed8b
inesgassiebayle <110676544+inesgassiebayle@users.noreply.github.com>
2024-09-19 17:10:42 -0300

    Add files via upload

---
+++ src/main/java/com/ingsis/jcli/snippets_service/SnippetsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.snippets_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsis/jcli/snippets_service/SnippetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.snippets_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
