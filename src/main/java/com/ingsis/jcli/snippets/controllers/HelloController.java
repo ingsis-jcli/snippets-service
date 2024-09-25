1fef74543cacd663e03f78eb69d315706f1304df
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-09-25 20:59:52 -0300

    docker file try 1

--- src/main/java/com/ingsis/jcli/snippets/controllers/HelloController.java
+++ src/main/java/com/ingsis/jcli/snippets/controllers/HelloController.java
@@ -1,8 +1,11 @@
 package com.ingsis.jcli.snippets.controllers;
 
+import com.ingsis.jcli.snippets.models.Hello;
+import com.ingsis.jcli.snippets.repositories.HelloRepository;
 import com.ingsis.jcli.snippets.services.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
   private final HelloService helloService;
+  private final HelloRepository helloRepository;
 
   @Autowired
-  public HelloController(HelloService helloService) {
+  public HelloController(HelloService helloService, HelloRepository helloRepository) {
     this.helloService = helloService;
+    this.helloRepository = helloRepository;
+  }
+
+  @PostMapping("/create")
+  public Hello createHello() {
+    // Create a new Hello entity and save it to the database
+    Hello hello = new Hello();
+    helloRepository.save(hello);
+    return hello;
   }
 
   @GetMapping
