7128b30cc0b5eeaf82de80cec2e789ed00e86a3d
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-09-26 23:16:21 -0300

    compose

--- src/main/java/com/ingsis/jcli/snippets/SnippetsServiceApplication.java
+++ src/main/java/com/ingsis/jcli/snippets/SnippetsServiceApplication.java
@@ -14,7 +14,7 @@ public class SnippetsServiceApplication {
   }
 
   private static void loadEnv() {
-    Dotenv dotenv = Dotenv.load();
+    Dotenv dotenv = Dotenv.configure().directory("./").load();
     System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
     System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
     System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
--- src/main/java/com/ingsis/jcli/snippets/clients/PermissionsClient.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/PermissionsClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "permissions", url = "http://localhost:8081/")
+@FeignClient(value = "permissions", url = "http://permissions-service:8081/")
 public interface PermissionsClient {
 
   @RequestMapping(method = RequestMethod.GET, value = "/hello")
