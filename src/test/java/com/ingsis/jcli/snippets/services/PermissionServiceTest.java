b31fa08d39dd74bfa5c8ba6b5d67595e5718cc5e
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-02 22:15:26 -0300

    Implement PermissionService tests

---
+++ src/test/java/com/ingsis/jcli/snippets/services/PermissionServiceTest.java
@@ -0,0 +1,71 @@
+package com.ingsis.jcli.snippets.services;
+
+import com.ingsis.jcli.snippets.clients.PermissionsClient;
+import com.ingsis.jcli.snippets.common.PermissionType;
+import com.ingsis.jcli.snippets.models.Snippet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PermissionServiceTest {
+
+  @Autowired
+  private PermissionService permissionService;
+
+  @MockBean
+  private SnippetService snippetService;
+
+  @MockBean
+  private PermissionsClient permissionsClient;
+
+  @Test
+  public void hasPermissionOwner() {
+    Long snippetId = 1L;
+    Long userId = 123L;
+    Snippet snippet = new Snippet("name", "url", userId);
+    snippet.setId(snippetId);
+
+    when(snippetService.isOwner(snippetId, userId)).thenReturn(true);
+    assertTrue(permissionService.hasPermissionOnSnippet(PermissionType.READ, snippetId, userId));
+  }
+
+  @Test
+  public void hasPermissionOther() {
+    Long snippetId = 1L;
+    Long ownerId = 345L;
+    Long userId = 123L;
+    Snippet snippet = new Snippet("name", "url", ownerId);
+    snippet.setId(snippetId);
+
+    when(snippetService.isOwner(snippetId, userId)).thenReturn(false);
+    when(permissionsClient.hasPermission(PermissionType.READ.name, snippetId, userId))
+        .thenReturn(ResponseEntity.ok(true));
+
+    assertTrue(permissionService.hasPermissionOnSnippet(PermissionType.READ, snippetId, userId));
+    assertNotEquals(ownerId, userId);
+  }
+
+  @Test
+  public void hasPermissionFalse() {
+    Long snippetId = 1L;
+    Long ownerId = 345L;
+    Long userId = 123L;
+    Snippet snippet = new Snippet("name", "url", ownerId);
+    snippet.setId(snippetId);
+
+    when(snippetService.isOwner(snippetId, userId)).thenReturn(false);
+    when(permissionsClient.hasPermission(PermissionType.READ.name, snippetId, userId))
+        .thenReturn(ResponseEntity.ok(false));
+
+    assertFalse(permissionService.hasPermissionOnSnippet(PermissionType.READ, snippetId, userId));
+    assertNotEquals(ownerId, userId);
+  }
+}
