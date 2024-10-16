2a5f58e55eee04e9d56534bd59f228133c9d9ff9
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-16 10:45:04 -0300

    AUTH0 SET UP

---
+++ src/main/java/com/ingsis/jcli/snippets/auth0/AudienceValidator.java
@@ -0,0 +1,28 @@
+package com.ingsis.jcli.snippets.auth0;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.List;
+
+public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+
+  private final String audience;
+
+  public AudienceValidator(String audience) {
+    this.audience = audience;
+  }
+
+  @Override
+  public OAuth2TokenValidatorResult validate(Jwt jwt) {
+    OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
+
+    List<String> jwtAudience = jwt.getAudience();
+    if (jwtAudience != null && jwtAudience.contains(audience)) {
+      return OAuth2TokenValidatorResult.success();
+    } else {
+      return OAuth2TokenValidatorResult.failure(error);
+    }
+  }
+}
