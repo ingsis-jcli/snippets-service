6d2bb586cdde9d1c7b91f809752cdc5b47847f58
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-19 14:38:24 -0300

    Add auth interceptor to LanguageClientFactory

--- src/main/java/com/ingsis/jcli/snippets/clients/factory/LanguageClientFactory.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/factory/LanguageClientFactory.java
@@ -1,21 +1,32 @@
 package com.ingsis.jcli.snippets.clients.factory;
 
+import com.ingsis.jcli.snippets.auth0.AuthFeignInterceptor;
 import com.ingsis.jcli.snippets.clients.LanguageClient;
 import com.ingsis.jcli.snippets.common.Generated;
 import feign.Feign;
+import feign.RequestInterceptor;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Generated
 @Component
 public class LanguageClientFactory {
+  
+  private final RequestInterceptor authFeignInterceptor;
+  
+  @Autowired
+  public LanguageClientFactory(AuthFeignInterceptor authFeignInterceptor) {
+    this.authFeignInterceptor = authFeignInterceptor;
+  }
 
   public LanguageClient createClient(String baseUrl) {
     LanguageClient target =
         Feign.builder()
             .encoder(new GsonEncoder())
             .decoder(new GsonDecoder())
+            .requestInterceptor(authFeignInterceptor)
             .target(LanguageClient.class, baseUrl);
     return target;
   }
