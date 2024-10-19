bb062e7d07ca29cc36ccd113011764597da68ee9
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-19 17:40:52 -0300

    Change annotation from feign to mvc in LanguageClient

--- src/main/java/com/ingsis/jcli/snippets/clients/LanguageClient.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/LanguageClient.java
@@ -2,19 +2,18 @@ package com.ingsis.jcli.snippets.clients;
 
 import com.ingsis.jcli.snippets.common.requests.ValidateRequest;
 import com.ingsis.jcli.snippets.common.responses.ValidateResponse;
-import feign.Headers;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "language")
 public interface LanguageClient {
 
-  @RequestLine("GET /hello")
+  @GetMapping("hello")
   String hello();
-
-  @RequestLine("POST /validate")
-  @Headers("Content-Type: application/json")
+  
+  @PostMapping(value = "/validate", consumes = "application/json", produces = "application/json")
   ResponseEntity<ValidateResponse> validate(@RequestBody ValidateRequest validateRequest);
 }
