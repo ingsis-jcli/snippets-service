e60eda11cf2811a1400968a4cc515a7e2ea1fb38
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-19 17:39:16 -0300

    Add contract to createClient in LanguageClientFactory

--- src/main/java/com/ingsis/jcli/snippets/clients/factory/LanguageClientFactory.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/factory/LanguageClientFactory.java
@@ -3,22 +3,28 @@ package com.ingsis.jcli.snippets.clients.factory;
 import com.ingsis.jcli.snippets.auth0.AuthFeignInterceptor;
 import com.ingsis.jcli.snippets.clients.LanguageClient;
 import com.ingsis.jcli.snippets.common.Generated;
+import feign.Contract;
 import feign.Feign;
 import feign.RequestInterceptor;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 @Generated
 @Component
+@Import(FeignClientsConfiguration.class)
 public class LanguageClientFactory {
 
   private final RequestInterceptor authFeignInterceptor;
-
+  private final Contract feignContract;
+  
   @Autowired
-  public LanguageClientFactory(AuthFeignInterceptor authFeignInterceptor) {
+  public LanguageClientFactory(AuthFeignInterceptor authFeignInterceptor, Contract feignContract) {
     this.authFeignInterceptor = authFeignInterceptor;
+    this.feignContract = feignContract;
   }
 
   public LanguageClient createClient(String baseUrl) {
@@ -26,6 +32,7 @@ public class LanguageClientFactory {
         Feign.builder()
             .encoder(new GsonEncoder())
             .decoder(new GsonDecoder())
+            .contract(feignContract)
             .requestInterceptor(authFeignInterceptor)
             .target(LanguageClient.class, baseUrl);
     return target;
