140f76a2604319e1acf8421ce6d1318b42d37a02
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-19 04:22:43 -0300

    Change @PostMapping to @RequestLine on LanguageClient

--- src/main/java/com/ingsis/jcli/snippets/clients/LanguageClient.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/LanguageClient.java
@@ -2,14 +2,14 @@ package com.ingsis.jcli.snippets.clients;
 
 import com.ingsis.jcli.snippets.common.language.LanguageResponse;
 import com.ingsis.jcli.snippets.common.requests.ValidateRequest;
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "language")
 public interface LanguageClient {
 
-  @PostMapping("validate")
+  @RequestLine("POST /validate")
   ResponseEntity<LanguageResponse> validate(@RequestBody ValidateRequest validateRequest);
 }
