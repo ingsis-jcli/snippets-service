567af20e68497cf8350db70452b49dcbc3f3568f
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-19 23:30:17 -0300

    Add logging to FeignErrorDecoder and FeignDecoder

--- src/main/java/com/ingsis/jcli/snippets/clients/factory/FeignDecoder.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/factory/FeignDecoder.java
@@ -9,6 +9,9 @@ import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
@@ -18,7 +21,10 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.util.StreamUtils;
 
 @Generated
+@Slf4j
 public class FeignDecoder implements Decoder {
+
+  private static final Marker marker = MarkerFactory.getMarker("FEIGN_DECODER");
 
   private final SpringDecoder springDecoder;
 
@@ -34,18 +40,24 @@ public class FeignDecoder implements Decoder {
 
   @Override
   public Object decode(Response response, Type type) throws IOException, DecodeException {
+    log.info(marker, "Decoding response");
     if (response.body() == null
         || response.body().length() == null
         || response.body().length() == 0) {
+      log.info(marker, "Response body is empty");
+      log.info(marker, "Response status: " + response.status());
       return ResponseEntity.status(response.status()).build();
     }
 
     String bodyAsString =
         StreamUtils.copyToString(response.body().asInputStream(), StandardCharsets.UTF_8);
     if (bodyAsString.trim().isEmpty()) {
+      log.info(marker, "Response body is blank");
+      log.info(marker, "Response status: " + response.status());
       return ResponseEntity.status(response.status()).build();
     }
 
+    log.info(marker, "Response status: " + response.status());
     Object decodedBody = springDecoder.decode(response, type);
     return ResponseEntity.status(response.status()).body(decodedBody);
   }
--- src/main/java/com/ingsis/jcli/snippets/clients/factory/FeignErrorDecoder.java
+++ src/main/java/com/ingsis/jcli/snippets/clients/factory/FeignErrorDecoder.java
@@ -7,24 +7,35 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 
 @Generated
+@Slf4j
 public class FeignErrorDecoder implements ErrorDecoder {
+
+  private static final Marker marker = MarkerFactory.getMarker("FEIGN_ERROR_DECODER");
 
   @Override
   public Exception decode(String methodKey, Response response) {
+    log.info(marker, "Decoding an error response");
     try {
       String bodyAsString =
           response.body() != null
               ? StreamUtils.copyToString(response.body().asInputStream(), StandardCharsets.UTF_8)
               : "";
 
+      log.info(marker, "Response: " + bodyAsString);
+
       JsonObject json = new Gson().fromJson(bodyAsString, JsonObject.class);
 
       return new FeignException(ResponseEntity.status(response.status()).body(json));
     } catch (IOException e) {
+      log.error(marker, "Error while decoding error response");
+      log.error(marker, e.getMessage());
       return new FeignException(ResponseEntity.status(response.status()).body(null));
     }
   }
