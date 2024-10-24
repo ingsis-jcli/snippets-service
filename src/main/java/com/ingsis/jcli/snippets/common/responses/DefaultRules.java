bb2c81e99835c6bdcd607b14a73cd902484480cf
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-23 23:18:12 -0300

    get and update of linting and formatting rules.

--- src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRules.java
+++ src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRules.java
@@ -1,8 +1,5 @@
 package com.ingsis.jcli.snippets.common.responses;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ingsis.jcli.snippets.common.Generated;
 import java.util.List;
 
-@Generated
-public record DefaultRules(@JsonProperty("rules") List<DefaultRule> rules) {}
+public record DefaultRules(List<DefaultRule> rules) {}
