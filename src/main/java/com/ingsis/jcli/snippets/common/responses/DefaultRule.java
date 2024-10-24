0b1e63306c92ca01183bfadba32666ba68f6bd94
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-23 22:38:54 -0300

    get and update of linting and formatting rules.

--- src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRule.java
+++ src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRule.java
@@ -1,11 +1,17 @@
 package com.ingsis.jcli.snippets.common.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class DefaultRule {
+  @JsonProperty("name")
   private String name;
+
+  @JsonProperty("isActive")
   private boolean isActive;
+
+  @JsonProperty("value")
   private String value;
 
   public DefaultRule(String name, boolean isActive, String value) {
--- src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRules.java
+++ src/main/java/com/ingsis/jcli/snippets/common/responses/DefaultRules.java
@@ -1,7 +1,8 @@
 package com.ingsis.jcli.snippets.common.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ingsis.jcli.snippets.common.Generated;
 import java.util.List;
 
 @Generated
-public record DefaultRules(List<DefaultRule> rules) {}
+public record DefaultRules(@JsonProperty("rules") List<DefaultRule> rules) {}
