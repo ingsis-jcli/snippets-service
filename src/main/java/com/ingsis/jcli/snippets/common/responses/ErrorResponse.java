565c1ddb427bca1af734222cb0c0945fb140ef3b
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-25 12:35:25 -0300

    fix in the create and edit snippet

--- src/main/java/com/ingsis/jcli/snippets/common/responses/ErrorResponse.java
+++ src/main/java/com/ingsis/jcli/snippets/common/responses/ErrorResponse.java
@@ -1,6 +1,8 @@
 package com.ingsis.jcli.snippets.common.responses;
 
-import com.ingsis.jcli.snippets.common.Generated;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Generated
-public record ErrorResponse(String error) {}
+public record ErrorResponse(
+  @JsonProperty("error") String error
+) {}
+
