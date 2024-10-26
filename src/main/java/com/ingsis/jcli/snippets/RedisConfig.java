e44f88cf94c49337726ea8b59c68187384caf06a
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-26 17:26:42 -0300

    trying to fix redis stream

--- src/main/java/com/ingsis/jcli/snippets/RedisConfig.java
+++ src/main/java/com/ingsis/jcli/snippets/RedisConfig.java
@@ -5,17 +5,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+  public ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    return mapper;
   }
 
   @Bean
@@ -23,23 +23,8 @@ public class RedisConfig {
       RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
     RedisTemplate<String, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(connectionFactory);
-
-    Jackson2JsonRedisSerializer<Object> serializer =
-        new Jackson2JsonRedisSerializer<>(objectMapper.constructType(Object.class));
-
     template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(serializer);
-    template.setHashKeySerializer(new StringRedisSerializer());
-    template.setHashValueSerializer(serializer);
-    template.afterPropertiesSet();
-
+    template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
     return template;
-  }
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.registerModule(new JavaTimeModule());
-    return mapper;
   }
 }
