d02310cb6e191198036812fbfc7c649d186dbedd
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-26 17:18:48 -0300

    trying to fix redis stream

--- src/main/java/com/ingsis/jcli/snippets/producers/TestCaseRunProducer.java
+++ src/main/java/com/ingsis/jcli/snippets/producers/TestCaseRunProducer.java
@@ -1,7 +1,5 @@
 package com.ingsis.jcli.snippets.producers;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.ingsis.jcli.snippets.models.TestCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestCaseRunProducer extends JavaRedisStreamProducer {
 
+  // TODO CONSUMER IN PRINTSCRIPT-SERVICE
+
   @Autowired
   public TestCaseRunProducer(
       @Value("${test_case_stream.key}") String streamKey, RedisTemplate<String, String> redis) {
@@ -18,21 +18,6 @@ public class TestCaseRunProducer extends JavaRedisStreamProducer {
   }
 
   public void run(TestCase testCase) {
-    JsonObject jsonObject = new JsonObject();
-    jsonObject.addProperty("id", testCase.getId());
-    jsonObject.addProperty("snippetName", testCase.getSnippet().getName());
-    jsonObject.addProperty("url", testCase.getSnippet().getUrl());
-    JsonArray inputArray = new JsonArray();
-    for (String input : testCase.getInputs()) {
-      inputArray.add(input);
-    }
-    jsonObject.add("input", inputArray);
-    JsonArray outputArray = new JsonArray();
-    for (String output : testCase.getOutputs()) {
-      outputArray.add(output);
-    }
-    jsonObject.add("output", outputArray);
-    System.out.println("Producing test case run: " + jsonObject);
-    emit(jsonObject);
+    emit("Sending test case run for " + testCase.getId());
   }
 }
