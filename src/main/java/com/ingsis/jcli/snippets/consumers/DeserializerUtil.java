aeac14210ae385e254362262cf9ae2935ec87f52
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-10-27 13:51:27 -0300

    fixes for formatting update

--- src/main/java/com/ingsis/jcli/snippets/consumers/DeserializerUtil.java
+++ src/main/java/com/ingsis/jcli/snippets/consumers/DeserializerUtil.java
@@ -1,6 +1,7 @@
 package com.ingsis.jcli.snippets.consumers;
 
 import com.google.gson.Gson;
+import com.ingsis.jcli.snippets.common.responses.SnippetStatusUpdateProduct;
 import com.ingsis.jcli.snippets.common.responses.TestCaseResultProduct;
 
 public class DeserializerUtil {
@@ -8,5 +9,12 @@ public class DeserializerUtil {
     Gson gson = new Gson();
     TestCaseResultProduct testCaseResultProduct = gson.fromJson(json, TestCaseResultProduct.class);
     return testCaseResultProduct;
+  }
+
+  public static SnippetStatusUpdateProduct deserializeIntoSnippetStatusUpdate(String json) {
+    Gson gson = new Gson();
+    SnippetStatusUpdateProduct snippetStatusUpdateProduct =
+        gson.fromJson(json, SnippetStatusUpdateProduct.class);
+    return snippetStatusUpdateProduct;
   }
 }
--- src/test/java/com/ingsis/jcli/snippets/consumers/DeserializerUtilTest.java
+++ src/test/java/com/ingsis/jcli/snippets/consumers/DeserializerUtilTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.ingsis.jcli.snippets.common.requests.TestType;
+import com.ingsis.jcli.snippets.common.responses.SnippetStatusUpdateProduct;
 import com.ingsis.jcli.snippets.common.responses.TestCaseResultProduct;
+import com.ingsis.jcli.snippets.common.status.ProcessStatus;
 import org.junit.jupiter.api.Test;
 
 public class DeserializerUtilTest {
@@ -13,15 +15,33 @@ public class DeserializerUtilTest {
   void whenValidJsonProvided_thenCorrectlyDeserializeToTestCaseResultProduct() {
     String json =
         """
-        {
-          "testCaseId": 12345,
-          "type": "VALID"
-        }
+      {
+        "testCaseId": 12345,
+        "type": "VALID"
+      }
         """;
 
     TestCaseResultProduct result = DeserializerUtil.deserializeIntoTestResult(json);
     assertNotNull(result);
     assertEquals(12345L, result.getTestCaseId());
     assertEquals(TestType.VALID, result.getType());
+  }
+
+  @Test
+  void whenValidJsonProvided_thenCorrectlyDeserializeToSnippetStatusUpdateProduct() {
+    String json =
+        """
+      {
+        "snippetId": 67890,
+        "operation": "FORMAT",
+        "status": "NON_COMPLIANT"
+      }
+        """;
+
+    SnippetStatusUpdateProduct result = DeserializerUtil.deserializeIntoSnippetStatusUpdate(json);
+    assertNotNull(result);
+    assertEquals(67890L, result.getSnippetId());
+    assertEquals("FORMAT", result.getOperation());
+    assertEquals(ProcessStatus.NON_COMPLIANT, result.getStatus());
   }
 }
