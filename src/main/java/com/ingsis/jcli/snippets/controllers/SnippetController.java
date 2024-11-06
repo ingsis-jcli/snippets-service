45beb1310bf40c5ecc6c622cd619d95a223ef271
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-11-06 20:29:30 -0300

    removing special characters in endpoint name

--- src/main/java/com/ingsis/jcli/snippets/controllers/SnippetController.java
+++ src/main/java/com/ingsis/jcli/snippets/controllers/SnippetController.java
@@ -86,11 +86,10 @@ public class SnippetController {
   @PostMapping()
   public ResponseEntity<Long> createSnippet(
       @RequestBody @Valid SnippetDto snippetDto,
-      @RequestParam(value = "version", defaultValue = "1.1") String version,
       @RequestHeader("Authorization") String token) {
 
     String userId = jwtService.extractUserId(token);
-    snippetDto.setVersion(version);
+    snippetDto.setVersion(snippetDto.getVersion());
 
     Snippet snippet = snippetService.createSnippet(snippetDto, userId);
     return new ResponseEntity<>(snippet.getId(), HttpStatus.CREATED);
