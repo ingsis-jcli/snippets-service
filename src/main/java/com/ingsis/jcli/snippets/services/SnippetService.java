a479e4df098e6f0bb167342ff34e0c83b0ffbaa0
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-11-11 15:57:40 -0300

    check no duplicates of space between names or without spaces

--- src/main/java/com/ingsis/jcli/snippets/services/SnippetService.java
+++ src/main/java/com/ingsis/jcli/snippets/services/SnippetService.java
@@ -86,7 +86,9 @@ public class SnippetService {
     if (!snippetRepository.findAllByNameAndOwner(snippetDto.getName(), userId).isEmpty()) {
       throw new InvalidSnippetException("Snippet with the same name already exists", null);
     }
-    if (!snippetRepository.findAllByNameAndOwner(snippetDto.getName().replaceAll("[\\s-]+", ""), userId).isEmpty()) {
+    if (!snippetRepository
+        .findAllByNameAndOwner(snippetDto.getName().replaceAll("[\\s-]+", ""), userId)
+        .isEmpty()) {
       throw new InvalidSnippetException("Snippet with the same name already exists", null);
     }
     saveInBucket(snippetDto, userId);
